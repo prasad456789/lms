c7fb631b31041aaddeaa36c728eafb1f4e6de574
prasad456789 <iiittstudent755@gmail.com>
2024-08-14 15:36:19 +0530

    Update settings.py

--- LMS/LMS/settings.py
+++ LMS/LMS/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    ‘whitenoise.middleware.WhiteNoiseMiddleware’,
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
