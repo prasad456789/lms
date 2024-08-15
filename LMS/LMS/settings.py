e46d95c68ee1f1ea8db4611de8105f56fe45389c
prasad456789 <iiittstudent755@gmail.com>
2024-08-15 15:34:22 +0530

    Update settings.py

--- LMS/LMS/settings.py
+++ LMS/LMS/settings.py
@@ -132,11 +132,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+#STATIC_URL = '/static/'
+#STATIC_ROOT = '/static/'
+
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR,'static')
+#]
+# settings.py
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = '/home/ubuntu/lms/staticfiles'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
