f443fecd5645506d5b9a195fd9aec8d85c375fd2
prasad456789 <iiittstudent755@gmail.com>
2024-08-15 09:18:01 +0530

    Update settings.py

--- LMS/LMS/settings.py
+++ LMS/LMS/settings.py
@@ -80,22 +80,22 @@ WSGI_APPLICATION = 'LMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'NAME': 'django',
-       'USER': 'admin',
-       'PASSWORD': 'password',
-       'HOST': 'localhost',
-       'PORT': '5432',
-    }
-}   
 #DATABASES = {
-#   'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}  
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#       'NAME': 'django',
+#       'USER': 'admin',
+#       'PASSWORD': 'password',
+#       'HOST': 'localhost',
+#       'PORT': '5432',
+#   }
+#}   
+DATABASES = {
+   'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}  
 
 
 # Password validation
